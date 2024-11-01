bd8a07c30b2b3791d8b9d11051808481181be200
sutrisnoadit <sutrisnoadit17@gmail.com>
2024-11-01 13:45:33 +0700

    tugas

---
+++ Demo/T1/odd/index.php
@@ -0,0 +1,19 @@
+<?php
+function printNumbers(int $n) {
+    for ($i = 1; $i <= $n; $i++) {
+        if ($i % 4 == 0 && $i % 6 == 0) {
+            echo "PemrogramanWebsite 2024\n";
+        } elseif ($i % 5 == 0) {
+            echo "2024\n";
+        } elseif ($i % 4 == 0) {
+            echo "Pemrograman\n";
+        } elseif ($i % 6 == 0) {
+            echo "Website\n";
+        } else {
+            echo $i . "\n";
+        }
+    }
+}
+
+
+printNumbers(30);
